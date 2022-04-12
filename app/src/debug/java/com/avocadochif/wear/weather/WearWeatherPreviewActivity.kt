7716fa34eb9a19366435d1e42f26f0107714a469
avocadochif <avocadochif@gmail.com>
2022-04-12 20:45:27 +0300

    Added base implementation of TileService and PreviewActivity.

---
+++ app/src/debug/java/com/avocadochif/wear/weather/WearWeatherPreviewActivity.kt
@@ -0,0 +1,31 @@
+package com.avocadochif.wear.weather
+
+import android.content.ComponentName
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.ComponentActivity
+import androidx.wear.tiles.manager.TileUiClient
+
+class WearWeatherPreviewActivity : ComponentActivity() {
+
+    private var tileUiClient: TileUiClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wear_weather_preview)
+
+        tileUiClient = TileUiClient(
+            context = this,
+            component = ComponentName(this, WearWeatherService::class.java),
+            parentView = findViewById<FrameLayout>(R.id.root)
+        ).also {
+            it.connect()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tileUiClient?.close()
+    }
+
+}
