981250d8672022788cb8fd6490f5b9b7f7762773
avocadochif <avocadochif@gmail.com>
2022-04-12 18:27:18 +0300

    initial commit

---
+++ app/src/main/java/com/avocadochif/wear/weather/WearWeatherService.kt
@@ -0,0 +1,19 @@
+package com.avocadochif.wear.weather
+
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.ResourceBuilders
+import androidx.wear.tiles.TileBuilders
+import androidx.wear.tiles.TileService
+import com.google.common.util.concurrent.ListenableFuture
+
+class WearWeatherService : TileService() {
+
+    override fun onTileRequest(requestParams: RequestBuilders.TileRequest): ListenableFuture<TileBuilders.Tile> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onResourcesRequest(requestParams: RequestBuilders.ResourcesRequest): ListenableFuture<ResourceBuilders.Resources> {
+        TODO("Not yet implemented")
+    }
+
+}
